492d10748a6ba3cad208875c91568fbe09e9ce5f
remdalm <85871079+remdalm@users.noreply.github.com>
2023-03-03 22:14:01 +0200

    get and parse users

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,54 @@
-use auth::config::MsOAuthConfig;
-use auth::get_access_token;
+use auth::config::MsGraphOAuthConfig;
+use oauth2::{AccessToken, TokenResponse};
+use serde::{Deserialize, Serialize};
 
 pub mod auth;
 
-pub fn run(ms_auth_config: MsOAuthConfig) -> Result<(), Box<dyn std::error::Error>> {
-    let access_token = auth::get_access_token(ms_auth_config)?;
+pub fn run(ms_auth_config: MsGraphOAuthConfig) -> Result<(), Box<dyn std::error::Error>> {
+    let binding = auth::get_access_token(ms_auth_config)?;
+    let access_token = binding.access_token();
+    let client = reqwest::blocking::Client::new();
+
+    list_users(&client, &access_token)?;
 
     Ok(())
+}
+
+// List users in the tenant using the Microsoft Graph API
+fn list_users(
+    client: &reqwest::blocking::Client,
+    access_token: &AccessToken,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let url = "https://graph.microsoft.com/beta/users";
+    let response = client.get(url).bearer_auth(access_token.secret()).send()?;
+
+    if !response.status().is_success() {
+        return Err(format!("request failed: {}", response.status()).into());
+    }
+
+    println!("response: {:#?}", response);
+
+    let parsed_response: MsGraphBetaBody<MsGraphUser> = response.json()?;
+    //let parsed_response: serde_json::Value = response.json()?;
+
+    println!("response: {:#?}", parsed_response);
+
+    Ok(())
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct MsGraphUser {
+    id: String,
+    #[serde(rename = "displayName")]
+    display_name: Option<String>,
+    mail: Option<String>,
+    // #[serde(rename = "userType")]
+    // user_type: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct MsGraphBetaBody<T> {
+    #[serde(rename = "@odata.context")]
+    odata_context: String,
+    value: Vec<T>,
 }
