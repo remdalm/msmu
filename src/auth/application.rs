ba4d36d02761fcd6196081a91a96924f226157b0
remdalm <85871079+remdalm@users.noreply.github.com>
2023-03-06 12:28:04 +0200

    add some clean up

--- src/auth/application.rs
+++ src/auth/application.rs
@@ -1,11 +1,8 @@
 use oauth2::basic::{BasicClient, BasicTokenType};
 use oauth2::reqwest::http_client;
 use oauth2::{
-    AuthUrl, ClientId, ClientSecret, EmptyExtraTokenFields, Scope, StandardTokenResponse,
-    TokenResponse, TokenUrl,
+    AuthUrl, ClientId, ClientSecret, EmptyExtraTokenFields, Scope, StandardTokenResponse, TokenUrl,
 };
-use reqwest::header::AUTHORIZATION;
-use serde::{Deserialize, Serialize};
 
 use super::config::MsGraphOAuthConfig;
 
@@ -16,8 +13,6 @@ pub fn get_access_token(
     let graph_client_id = ClientId::new(ms_auth_config.graph_client_id);
     let graph_client_secret = ClientSecret::new(ms_auth_config.graph_client_secret);
 
-    let auth_url =
-        AuthUrl::new("https://fake.com".to_string()).expect("Invalid authorization endpoint URL");
     let token_url = TokenUrl::new(format!(
         "https://login.microsoftonline.com/{}/oauth2/v2.0/token",
         ms_auth_config.tenant_id
@@ -37,7 +32,7 @@ pub fn get_access_token(
     let client = BasicClient::new(
         graph_client_id,
         Some(graph_client_secret),
-        auth_url,
+        AuthUrl::new("https://fake.com".to_string()).unwrap(), // not used with client credentials protocol
         Some(token_url),
     );
 
@@ -47,33 +42,6 @@ pub fn get_access_token(
             "https://graph.microsoft.com/.default".to_string(),
         ))
         .request(http_client)?;
-
-    // let client = reqwest::blocking::Client::new();
-    // let url = "https://login.microsoftonline.com/organization/oauth2/v2.0/token";
-    // let token_result = client
-    //     .post(url)
-    //     .form(&[
-    //         ("client_id", graph_client_id.as_str()),
-    //         ("client_secret", graph_client_secret.secret()),
-    //         ("grant_type", "client_credentials"),
-    //         ("scope", "https://graph.microsoft.com/.default"),
-    //     ])
-    //     .send()?;
-
-    // let token_result = client
-    //     .post(url)
-    //     .basic_auth(graph_client_id.as_str(), Some(graph_client_secret.secret()))
-    //     .form(&[("grant_type", "client_credentials"), ("scope", scope)])
-    //     .send();
-
-    // let url = format!(
-    //     "https://login.microsoftonline.com/organization/oauth2/v2.0/token?client_id={}&client_secret={}&grant_type=client_credentials&scope={}",
-    //     graph_client_id.as_str(),
-    //     graph_client_secret.secret(),
-    //     scope
-    // );
-
-    // // let token_result = reqwest::blocking::get(url)
 
     Ok(token_result)
 }
