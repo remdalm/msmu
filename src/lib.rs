fedeaca3180c106e3c1e3d2811e2937a45fa5ad8
remdalm <85871079+remdalm@users.noreply.github.com>
2023-03-06 23:26:47 +0200

    Sort out MsUser

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,53 @@ use oauth2::{AccessToken, TokenResponse};
 use serde::{Deserialize, Serialize};
 
 pub mod auth;
+#[derive(Debug)]
+enum UserPurpose {
+    User,
+    Linked,
+    Shared,
+    Room,
+    Equipment,
+    Others,
+    UnknownFutureValue,
+}
+
+impl UserPurpose {
+    fn from_str(s: &str) -> Self {
+        match s {
+            "user" => UserPurpose::User,
+            "linked" => UserPurpose::Linked,
+            "shared" => UserPurpose::Shared,
+            "room" => UserPurpose::Room,
+            "equipment" => UserPurpose::Equipment,
+            "others" => UserPurpose::Others,
+            "unknownFutureValue" => UserPurpose::UnknownFutureValue,
+            _ => panic!("Unknown user purpose: {}", s),
+        }
+    }
+}
 
 pub fn run(ms_auth_config: MsGraphOAuthConfig) -> Result<(), Box<dyn std::error::Error>> {
     let binding = auth::application::get_access_token(ms_auth_config)?;
+    println!("Auth Sucessful!");
     let access_token = binding.access_token();
     let client = reqwest::blocking::Client::new();
 
-    list_users(&client, &access_token)?;
+    let raw_users = list_users(&client, &access_token)?;
+
+    let mut users: Vec<User> = Vec::new();
+    let mut shared_mailboxes: Vec<SharedMailbox> = Vec::new();
+    // loop raw_users and print get_user_purpose for each user
+    for user in raw_users {
+        let user_purpose = get_user_purpose(&client, &access_token, &user.id)?;
+        match user_purpose {
+            Some(UserPurpose::User) => users.push(User::from_ms_graph_user(user)),
+            Some(UserPurpose::Shared) => {
+                shared_mailboxes.push(SharedMailbox::from_ms_graph_user(user))
+            }
+            _ => {}
+        }
+    }
 
     Ok(())
 }
@@ -18,29 +58,32 @@ pub fn run(ms_auth_config: MsGraphOAuthConfig) -> Result<(), Box<dyn std::error:
 fn list_users(
     client: &reqwest::blocking::Client,
     access_token: &AccessToken,
-) -> Result<(), Box<dyn std::error::Error>> {
+) -> Result<(Vec<MsGraphUser>), Box<dyn std::error::Error>> {
     let url = "https://graph.microsoft.com/beta/users";
     let response = client.get(url).bearer_auth(access_token.secret()).send()?;
 
     if !response.status().is_success() {
-        return Err(format!("request failed: {}", response.status()).into());
+        return Err(format!(
+            "request failed in list_users function: {}",
+            response.status()
+        )
+        .into());
     }
 
-    println!("response: {:#?}", response);
+    let parsed_response: MsGraphBetaBody<Vec<MsGraphUser>> = response.json()?;
 
-    let parsed_response: MsGraphBetaBody<MsGraphUser> = response.json()?;
-    //let parsed_response: serde_json::Value = response.json()?;
-
-    println!("response: {:#?}", parsed_response);
-
-    Ok(())
+    Ok(parsed_response
+        .value
+        .into_iter()
+        .filter(|user| user.mail.is_some())
+        .collect())
 }
 
 fn get_user_purpose(
     client: &reqwest::blocking::Client,
     access_token: &AccessToken,
     guid: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
+) -> Result<Option<UserPurpose>, Box<dyn std::error::Error>> {
     let url = format!(
         "https://graph.microsoft.com/beta/users/{}/mailboxSettings/userPurpose",
         guid
@@ -48,17 +91,19 @@ fn get_user_purpose(
     let response = client.get(url).bearer_auth(access_token.secret()).send()?;
 
     if !response.status().is_success() {
-        return Err(format!("request failed: {}", response.status()).into());
+        if response.status() == reqwest::StatusCode::NOT_FOUND {
+            return Ok(None);
+        }
+        return Err(format!(
+            "request failed in get_user_purpose function: {}",
+            response.status()
+        )
+        .into());
     }
 
-    println!("response: {:#?}", response);
+    let parsed_response: MsGraphBetaBody<String> = response.json()?;
 
-    // let parsed_response: MsGraphBetaBody<Option<String>> = response.json()?;
-    let parsed_response: serde_json::Value = response.json()?;
-
-    println!("response: {:#?}", parsed_response);
-
-    Ok(())
+    Ok(Some(UserPurpose::from_str(&parsed_response.value)))
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -67,13 +112,55 @@ struct MsGraphUser {
     #[serde(rename = "displayName")]
     display_name: Option<String>,
     mail: Option<String>,
-    // #[serde(rename = "userType")]
-    // user_type: Option<String>,
 }
 
 #[derive(Debug, Deserialize)]
 struct MsGraphBetaBody<T> {
     #[serde(rename = "@odata.context")]
     odata_context: String,
-    value: Vec<T>,
+    value: T,
+}
+#[derive(Debug, Serialize, Deserialize)]
+struct MsGraphSharedMailbox {
+    id: String,
+    #[serde(rename = "displayName")]
+    display_name: Option<String>,
+    mail: Option<String>,
+}
+
+trait FromMsGraphUser {
+    fn from_ms_graph_user(user: MsGraphUser) -> Self;
+}
+#[derive(Debug)]
+struct User {
+    id: String,
+    display_name: Option<String>,
+    mail: String,
+}
+
+impl FromMsGraphUser for User {
+    fn from_ms_graph_user(user: MsGraphUser) -> Self {
+        User {
+            id: user.id,
+            display_name: user.display_name,
+            mail: user.mail.unwrap(),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct SharedMailbox {
+    id: String,
+    display_name: Option<String>,
+    mail: String,
+}
+
+impl FromMsGraphUser for SharedMailbox {
+    fn from_ms_graph_user(user: MsGraphUser) -> Self {
+        SharedMailbox {
+            id: user.id,
+            display_name: user.display_name,
+            mail: user.mail.unwrap(),
+        }
+    }
 }
